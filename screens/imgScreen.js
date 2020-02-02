dac1decb0efe7b9311d2fbdb8dc5b869cddc7354
lordprotein <core.safecrlf>
2020-02-02 11:54:51 +0500

    add download icon

--- screens/imgScreen.js
+++ screens/imgScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, FlatList, Image, Alert } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, FlatList, Image, Alert, ActivityIndicator } from 'react-native';
 import Service from '../service/service';
 import { CardImg } from '../components/CardImg/CardImg';
 import { FindPanel } from '../components/FindPanel/FindPanel';
@@ -7,112 +7,125 @@ import { FindPanel } from '../components/FindPanel/FindPanel';
 
 
 export const ImgScreen = props => {
-   const [imgList, setImgList] = useState(); //For actual list with img
-   const [numLastImg, setNumLastImg] = useState(1); //For lazy load in "All" img
-   const [isFilter, setIsFilter] = useState(false); //For check staus filter ( is try - delete lazyload)
-   const [titleAlbum, setTitleAlbum] = useState('All'); //For title album
+    const [imgList, setImgList] = useState(); //For actual list with img
+    const [numLastImg, setNumLastImg] = useState(1); //For lazy load in "All" img
+    const [isFilter, setIsFilter] = useState(false); //For check staus filter ( is try - delete lazyload)
+    const [titleAlbum, setTitleAlbum] = useState('All'); //For title album
+    const [isDownload, setIsDownload] = useState(true);
 
-   const service = new Service();
-
-
-   useEffect(() => {
-      setNewListAllPhoto()
-   }, []);
+    const service = new Service();
 
 
-   const setNewListAllPhoto = () => {  //Clean download all img
-      service.getPhotoListCertainLong(1)
-         .then(res => {
-            if (!res) return;
-
-            setImgList(res.dataList); //Start list img
-            setNumLastImg(res.lastNum); //For lazyload
-            setTitleAlbum('All'); //Default name album with all img
-            setIsFilter(false); //False edit
-         })
-   }
+    useEffect(() => {
+        setNewListAllPhoto()
+    }, []);
 
 
-   const loadMorePhoto = () => { //Authomatic download img when scroll to end (lazyload)
-      service.getPhotoListCertainLong(numLastImg)
-         .then(res => {
-            if (!res) return;
+    const setNewListAllPhoto = () => {  //Clean download all img
+        setIsDownload(true);
+        service.getPhotoListCertainLong(1)
+            .then(res => {
+                if (!res) {
+                    setIsDownload(false);
+                    return;
+                }
 
-            setImgList([...imgList, ...res.dataList]);
-            setNumLastImg(res.lastNum);
-         });
-   }
-
-
-   const getTitleAlbum = id => { //get title album when using filter
-      service.getAlbumTitle(id)
-         .then(res => setTitleAlbum(res));
-   }
-
-
-   const getFilteredData = (albumId) => { //create new data for filtered data
-      albumId = parseInt(albumId);
-      if (!albumId) {
-         Alert.alert('Please, enter number without letters and symbols')
-         return;
-      };
-
-      service.getPhotoInAlbum(albumId)
-         .then(res => {
-            if (!res.length) {
-               Alert.alert('Haven\'t this album');
-               return;
-            }
-
-            setImgList(res);
-
-            setNumLastImg(1);
-
-            getTitleAlbum(albumId);
-
-            setIsFilter(true);
-         });
-   }
-
-   const renderItem = ({ item }) => {
-      const { url, title } = item;
-      return (
-         <CardImg
-            link={url}
-            title={title}
-         />
-      );
-   }
+                setImgList(res.dataList); //Start list img
+                setNumLastImg(res.lastNum); //For lazyload
+                setTitleAlbum('All'); //Default name album with all img
+                setIsFilter(false); //False edit
+                setIsDownload(false);
+            })
+    }
 
 
+    const loadMorePhoto = () => { //Authomatic download img when scroll to end (lazyload)
+        setIsDownload(true);
+        service.getPhotoListCertainLong(numLastImg)
+            .then(res => {
+                if (!res) {
+                    setIsDownload(false);
+                    return;
+                }
 
-   return (
-      <View>
-         <FindPanel
-            toFind={num => getFilteredData(num)}
-            getAll={setNewListAllPhoto}
-         />
+                setImgList([...imgList, ...res.dataList]);
+                setNumLastImg(res.lastNum);
+                setIsDownload(false);
+            });
+    }
 
-         <Text style={styles.title}>{titleAlbum}</Text>
 
-         <FlatList
-            data={imgList}
-            renderItem={item => renderItem(item)}
-            keyExtractor={item => `${item.id}`}
-            onEndReachedThreshold={!isFilter && 0.5}
-            onEndReached={!isFilter && loadMorePhoto}
-         />
-      </View>
-   );
+    const getTitleAlbum = id => { //get title album when using filter
+        service.getAlbumTitle(id)
+            .then(res => setTitleAlbum(res));
+    }
+
+
+    const getFilteredData = (albumId) => { //create new data for filtered data
+        albumId = parseInt(albumId);
+        if (!albumId) {
+            Alert.alert('Please, enter number without letters and symbols')
+            return;
+        };
+
+        service.getPhotoInAlbum(albumId)
+            .then(res => {
+                if (!res.length) {
+                    Alert.alert('Haven\'t this album');
+                    setIsDownload(false);
+                    return;
+                }
+
+                setImgList(res);
+                // setNumLastImg(1);
+                getTitleAlbum(albumId);
+                setIsFilter(true);
+                setIsDownload(false);
+            });
+    }
+
+    const renderItem = ({ item }) => {
+        const { url, title } = item;
+        return (
+            <CardImg
+                link={url}
+                title={title}
+            />
+        );
+    }
+
+
+
+    return (
+        <View>
+
+            <FindPanel
+                toFind={num => getFilteredData(num)}
+                getAll={setNewListAllPhoto}
+            />
+
+            <Text style={styles.title}>{titleAlbum}</Text>
+ 
+            {isDownload && <ActivityIndicator size="large" color="#0000ff" />}
+
+            <FlatList
+                data={imgList}
+                renderItem={item => renderItem(item)}
+                keyExtractor={item => `${item.id}`}
+                onEndReachedThreshold={!isFilter && 0.5}
+                onEndReached={!isFilter && loadMorePhoto}
+            />
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-   title: {
-      marginTop: 30,
-      marginBottom: 50,
-      fontSize: 25,
-      textAlign: "center",
-      color: '#626262'
-   }
-
+    title: {
+        marginTop: 30,
+        marginBottom: 50,
+        fontSize: 25,
+        textAlign: "center",
+        color: '#626262'
+    },
 });
--- screens/userScreen.js
+++ screens/userScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, FlatList } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, FlatList, ActivityIndicator } from 'react-native';
 import CardUser from '../components/CardUser/CardUser';
 import Service from '../service/service';
 
@@ -8,42 +8,53 @@ import Service from '../service/service';
 export const UserScreen = props => {
     const [userList, setUserList] = useState();
     const [numLastCard, setNumLastCard] = useState(1);
+    const [isDownload, setIsDownload] = useState(true);
     const service = new Service();
 
     useEffect(() => {
         service.getUserListCertainLong(numLastCard)
             .then(res => {
                 if (!res) return;
-
+                console.log(3)
                 setUserList(res.dataList);
                 setNumLastCard(res.lastNum);
+                setIsDownload(false);
             })
 
     }, []);
 
     const loadMoreUsers = () => {
+        setIsDownload(true);
         service.getUserListCertainLong(numLastCard)
             .then(res => {
-                if (!res) return;
+                if (!res) {
+                    setIsDownload(false);
+                    return;
+                }
 
                 setUserList([...userList, ...res.dataList]);
                 setNumLastCard(res.lastNum);
+                setIsDownload(false);
             });
     }
 
     const renderItem = ({ item }) => {
         return <CardUser cardData={item} />;
     }
-
+    console.log(isDownload)
     return (
-        <FlatList
-            data={userList}
-            renderItem={(item) => renderItem(item)}
-            onEndReachedThreshold={0.5}
-            onEndReached={loadMoreUsers}
-            refreshing={true}
-            keyExtractor={item => `${item.id}`}
-        />
+        <View>
+            {isDownload && <ActivityIndicator size="large" color="#0000ff" />}
+            <FlatList
+                data={userList}
+                renderItem={(item) => renderItem(item)}
+                onEndReachedThreshold={0.5}
+                onEndReached={loadMoreUsers}
+                refreshing={true}
+                keyExtractor={item => `${item.id}`}
+            />
+        </View>
+
     );
 }
 
