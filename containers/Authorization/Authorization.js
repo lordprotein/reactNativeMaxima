a2dde082fb98b97b432538e71e2bc0c12d88a9b8
lordprotein <core.safecrlf>
2020-02-02 18:52:14 +0500

    add styles and new inputs

--- containers/Authorization/Authorization.js
+++ containers/Authorization/Authorization.js
@@ -5,37 +5,74 @@ import {
     Text,
     View,
     Button,
-    TextInput
+    TextInput,
+    Alert
 } from 'react-native';
 
 export const Authorization = props => {
-    const [inputText, setInputText] = useState('');
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
 
 
-    const onChangeInput = text => {
-        setInputText(text);
+    const onChangeInput = (text, setNewState) => {
+        setNewState(text);
     }
 
 
     const _signInAsync = async () => {
-        if (inputText !== 'admin') return;
+        if (login !== 'admin' || password !== 'admin') {
+            Alert.alert('Incorrect login or password')
+            setLogin('');
+            setPassword('');
+            return;
+        };
 
-        await AsyncStorage.setItem('userTokin', inputText);
+        await AsyncStorage.setItem('authorization', 'true');
         props.navigation.navigate('Images');
-        
+
     }
 
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
+        <View style={styles.main}>
+            <TextInput
+                style={styles.input}
+                onChangeText={text => onChangeInput(text, setLogin)}
+                placeholder="login"
+                value={login}
+            />
+            <TextInput
+                style={styles.input}
+                onChangeText={text => onChangeInput(text, setPassword)}
+                secureTextEntry={true}
+                placeholder="password"
+                value={password}
+            />
             <Button
                 title="Log in"
                 onPress={_signInAsync}
             />
-            <TextInput
-                style={{ borderWidth: 1 }}
-                onChangeText={text => onChangeInput(text)}
-            />
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#E5E5E5",
+        paddingHorizontal: 40,
+        paddingVertical: 20,
+    },
+    input: {
+        marginBottom: 5,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        backgroundColor: '#FFF',
+        color: '#615151',
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: '#515151',
+    },
+});
