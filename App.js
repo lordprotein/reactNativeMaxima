66d573705286231a771503ccacad94235a10dcfe
lordprotein <core.safecrlf>
2020-02-02 13:04:42 +0500

    add authorization

--- App.js
+++ App.js
@@ -1,15 +1,50 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, Button, FlatList, ScrollView } from 'react-native';
 import { UserScreen } from './screens/userScreen';
+import { ImgScreen } from './screens/imgScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Authorization } from './containers/Authorization/Authorization';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	Button,
+	FlatList,
+	ScrollView
+} from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
 
+
+const AuthStack = createStackNavigator(
+	{
+		Images: ImgScreen,
+		Authorization: Authorization
+	},
+	{
+		initialRouteName: 'Authorization'
+	}
+)
+
+const TabNavigator = createBottomTabNavigator(
+	{
+		Users: UserScreen,
+		// Images: ImgScreen,
+		Auth: AuthStack,
+	},
+);
+
+const AppContainer = createAppContainer(TabNavigator);
 
 const App = () => {
-	return (
-		<View style={styles.main}>
-			<UserScreen />
-		</View>
-	);
+	return <AppContainer />;
 }
+
+
+
+
+
 
 
 const styles = StyleSheet.create({
---
+++ containers/Authorization/Authorization.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    AsyncStorage,
+    Text,
+    View,
+    Button,
+    TextInput
+} from 'react-native';
+
+export const Authorization = props => {
+    const [inputText, setInputText] = useState('');
+
+
+    const onChangeInput = text => {
+        setInputText(text);
+    }
+
+
+    const _signInAsync = async () => {
+        if (inputText !== 'admin') return;
+
+        await AsyncStorage.setItem('userTokin', inputText);
+        props.navigation.navigate('Images');
+    }
+
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Button
+                title="Log in"
+                onPress={_signInAsync}
+            />
+            <TextInput
+                style={{ borderWidth: 1 }}
+                onChangeText={text => onChangeInput(text)}
+            />
+        </View>
+    )
+}
